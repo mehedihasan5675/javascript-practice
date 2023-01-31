c45d2a879c62aa7905d21a0780284bca6b845cde
MD MEHEDI HASAN <mdchanchal5675@gmail.com>
2023-01-31 22:59:54 +0600

    function

---
+++ 3.function/func_object.js
@@ -0,0 +1,50 @@
+//Object ber korar niyom
+let humanHas = {
+    head:1,
+    eyes:2,
+    teeth:32,
+    bones:206,
+    fingure:10,
+    hand:2,
+    leg:2,
+    noise:1
+}
+// console.log(humanHas.head);
+// console.log(humanHas['eyes']);
+// let propertyName='teeth';
+// console.log(humanHas[propertyName]);
+
+
+
+//sob property name ak sathe array te dekhar niyon
+// let showAllPropertyName= Object.keys(humanHas);
+// console.log("showAllPropertyName", showAllPropertyName);
+
+
+//sob property value ak sathe dekhar niyon
+// let showAllValue = Object.values(humanHas);
+// console.log( showAllValue );
+
+
+//set property new value
+console.log(humanHas.bones=44);
+console.log(humanHas['teeth']=34);
+
+let changeFingureValue = 'fingure';
+console.log(humanHas[changeFingureValue]=12);
+
+
+
+ 
+
+
+
+
+
+// console.log(humanHas);
+
+
+
+
+
+
